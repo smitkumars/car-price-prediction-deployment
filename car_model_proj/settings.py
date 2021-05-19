b2fb360e4002f5535c2def7d6132da085f39c9f7
smitkumars1991 <smitkumars1991@gmail.com>
2021-05-19 13:02:45 +0530

    file name changed

--- car_model_proj/settings.py
+++ car_model_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m3ewh#r6*xf*qx@)6f=xzpfiv)7nu3usog_r^uot9m+*ew6oh$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1','carspricepredic.herokuapp.com']
 
 
 # Application definition
